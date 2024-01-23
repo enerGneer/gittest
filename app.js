25fd35c35bedb4b3abc67e34da6e60f4554d8c18
enerGneer <energneer@gmail.com>
2024-01-23 18:11:34 +0900

    Update: admin authorization

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const csrf = require('csurf');
-const expressSession = require('express-session');
+const express = require("express");
+const csrf = require("csurf");
+const expressSession = require("express-session");
 
-const createSessionConfig = require('./config/session');
-const db = require('./data/database');
-const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
-const errorHandlerMiddleware = require('./middlewares/error-handler');
-const checkAuthStatusMiddleware = require('./middlewares/check-auth');
-const authRoutes = require('./routes/auth.routes');
-const productsRoutes = require('./routes/products.routes');
-const baseRoutes = require('./routes/base.routes');
+const createSessionConfig = require("./config/session");
+const db = require("./data/database");
+const addCsrfTokenMiddleware = require("./middlewares/csrf-token");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const checkAuthStatusMiddleware = require("./middlewares/check-auth");
+const authRoutes = require("./routes/auth.routes");
+const productsRoutes = require("./routes/products.routes");
+const baseRoutes = require("./routes/base.routes");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 const sessionConfig = createSessionConfig();
@@ -40,6 +40,6 @@ db.connectToDatabase()
     app.listen(3000);
   })
   .catch(function (error) {
-    console.log('Failed to connect to the database!');
+    console.log("Failed to connect to the database!");
     console.log(error);
   });
